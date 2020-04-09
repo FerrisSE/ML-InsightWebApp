6a0af83bc568e0841246ae0e1e8be43a9083aa5a
Anthony Scott <scotta42@ferris.edu>
2020-04-09 16:35:55 -0400

    Started working on file converter

---
+++ WekaInsightAPI/src/weka/api/CSVtoArff.java
@@ -0,0 +1,28 @@
+package weka.api;
+
+//importing required classes//
+import weka.core.Instances;
+import weka.core.converters.ArffSaver;
+import weka.core.converters.CSVLoader;
+import java.io.File;
+//------------------------//
+
+
+public class CSVtoArff {
+	public static void main(String args[]) throws Exception{
+		//Load CSV
+		CSVLoader loader = new CSVLoader();
+		loader.setSource(new File("/CSVsamples/weather.numeric.csv"));
+		Instances data = loader.getDataSet();
+		
+		//save ARFF
+		ArffSaver saver = new ArffSaver();
+		saver.setInstances(data);	//set the dataset we want to convert
+		
+		//save as ARFF
+
+		saver.setFile(new File("/ArffSamples/weather.numeric.arff"));
+		saver.writeBatch();
+	}
+
+}
